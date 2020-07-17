49a2d30f41df4aa5886a41acafba4b02530ac54e
SilvenLEAF <SilvenLEAF@gmail.com>
2020-07-17 08:02:52 +0530

    fixed routes

--- shareyourstories/src/components/layout/SignedInLinks.js
+++ shareyourstories/src/components/layout/SignedInLinks.js
@@ -6,9 +6,9 @@ function SignedInLinks() {
   return (
     <>
       <li className = "hide-on-large-only"><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/">Create Story</NavLink></li>
-      <li><NavLink to="/">Log Out</NavLink></li>
-      <li className = "hide-on-large-only"><NavLink to="/">About Us</NavLink></li>
+      <li><NavLink to="/create">Create Story</NavLink></li>
+      <li><NavLink to="/signout">Log Out</NavLink></li>
+      <li className = "hide-on-large-only"><NavLink to="/about">About Us</NavLink></li>
       <li><NavLink to="/" className= "btn-floating pink pulse center hide-on-med-and-down">FZ</NavLink></li>
     </>
   )
--- shareyourstories/src/components/layout/SignedOutLinks.js
+++ shareyourstories/src/components/layout/SignedOutLinks.js
@@ -6,9 +6,9 @@ function SignedOutLinks() {
   return (
     <>
       <li className = "hide-on-large-only"><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/">Sign Up</NavLink></li>
-      <li><NavLink to="/">Log In</NavLink></li>
-      <li className = "hide-on-large-only"><NavLink to="/">About Us</NavLink></li>
+      <li><NavLink to="/signup">Sign Up</NavLink></li>
+      <li><NavLink to="/signin">Log In</NavLink></li>
+      <li className = "hide-on-large-only"><NavLink to="/about">About Us</NavLink></li>
     </>
   )
 }
